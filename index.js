0b3f18dc693c945db0166d771d7b68255cae44be
zihadx <blankzihad@gmail.com>
2023-08-18 12:14:34 +0600

    book post sucesfull

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 // Data-Base start
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.MONGODB_USER_NAME}:${process.env.MONGODB_PASSWORD}@book-verse.uifvr5z.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version--------
@@ -20,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,69 +30,63 @@ async function run() {
     const database = client.db("bookVerse");
     const allBooksCollections = database.collection("allBooks");
     const usersCollection = database.collection("users");
- 
-
-
-
 
     // get all books  start
 
-    app.get('/allBooks',async(req,res)=>{
+    app.get("/allBooks", async (req, res) => {
+      const result = await allBooksCollections.find().toArray();
 
-      
-
-         const result=await allBooksCollections.find().toArray()
-         
-         res.send(result)
-
-
-    })
-   // get all books  end
-
-
+      res.send(result);
+    });
+    // get all books  end
 
     // get single book by id  start
 
-    app.get('/singleBook/:id',async(req,res)=>{
+    app.get("/singleBook/:id", async (req, res) => {
+      const id = req.params.id;
 
-         const id = req.params.id
+      const find = { _id: new ObjectId(id) };
 
-         const find={_id : new ObjectId(id)}
+      const result = await allBooksCollections.findOne(find);
 
-         const result =await allBooksCollections.findOne(find)
-         
-         res.send(result)
+      res.send(result);
+    });
+    // get single book id  end
 
+    //user related api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
-    })
-   // get single book id  end
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
 
-//user related api
-app.get("/users", async (req, res) => {
-  const result = await usersCollection.find().toArray();
-  res.send(result);
-});
+      if (existingUser) {
+        return res.send({ message: "user exists" });
+      }
 
-app.post("/users", async (req, res) => {
-  const user = req.body;
-  console.log(user);
-  const query = { email: user.email };
-  const existingUser = await usersCollection.findOne(query);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
-  if (existingUser) {
-    return res.send({ message: "user exists" });
-  }
-
-  const result = await usersCollection.insertOne(user);
-  res.send(result);
-});
-
-
-
+    //------------------ Post method start------------------
+    app.post("/allBooks", async (req, res) => {
+      const newBook = req.body;
+      console.log(newBook);
+      const result = await allBooksCollections.insertOne(newBook);
+      res.send(result);
+    });
+    //------------------ Post method end------------------
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -101,15 +94,6 @@ app.post("/users", async (req, res) => {
 }
 run().catch(console.dir);
 // Data-Base end
-
-
-
-
-
-
-
-
-
 
 app.get("/", (req, res) => {
   res.send("Book Verse server is running");
