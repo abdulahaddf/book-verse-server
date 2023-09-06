e800920cc3eb3f6d3bc97e9e71dbc4d429596224
tonutonmoy <www.tonutonmoy12@gmail.com>
2023-09-06 15:00:16 +0600

    ok done bro0000

--- index.js
+++ index.js
@@ -58,6 +58,18 @@ async function run() {
     const paymentCollection = database.collection("payments");
     const oldBooksCollection = database.collection("oldBooks");
     const bestSellingAndRecentSelling = database.collection("bestSellingAndRecentSelling");
+    const userToUser = database.collection("userToUser")
+
+
+    const result = await userToUser.createIndex({ seller: 1, buyer: 1 }, { unique: true });
+
+    // Check the result of the index creation start by Tonmoy
+    if (result) {
+      console.log('Unique index on seller and buyer fields created.');
+    } else {
+      console.error('Error creating unique index on seller and buyer fields.');
+    }
+    // Check the result of the index creation end by Tonmoy
 
 
 
@@ -736,7 +748,9 @@ async function run() {
 
     //  post data SSLCommerz end  by Tonmoy -----------------------------------------------
 
-    // Real time Chat start by Tonmoy-------------------------------------------------------
+
+
+    // Real time Chat Admin to Users start by Tonmoy-------------------------------------------------------
 
 
 
@@ -811,13 +825,13 @@ async function run() {
         const email = req?.query?.email;
         const cancel = 'cancel';
 
-        
+
 
         if (!email) {
           return res.status(400).send('Email parameter is missing');
         }
 
-       
+
         const userDocument = await usersCollection.findOne({ email });
 
         if (!userDocument) {
@@ -826,7 +840,7 @@ async function run() {
 
         const lastMessageIndex = userDocument.chat.length - 1;
 
-        
+
         const filter = { email };
         const updateDoc = {
           $set: {
@@ -971,7 +985,216 @@ async function run() {
 
 
 
-    //  Real time Chat end by Tonmoy----------------------------------------------------------
+    //  Real time Chat Admin to Users end by Tonmoy----------------------------------------------------------
+
+    //  Real time Chat User to User end by Tonmoy----------------------------------------------------------
+
+
+    // create the seller And Buyer Collections 
+    app.post('/sellerAndBuyerCollections', async (req, res) => {
+      try {
+
+        const { seller, buyer } = req.body;
+
+        if (!seller || !buyer) {
+
+          return res.status(500).json({ error: 'seller and buyer missing ' });
+        }
+
+        const info = req.body;
+        console.log(info);
+
+
+        const checkingInfo1 = await userToUser.findOne({ $and: [{ seller: seller }, { buyer: buyer }] });
+
+
+        if (checkingInfo1) {
+          console.log('checkingInfo1')
+          return res.status(400).json({ error: 'A record with this seller and buyer already exists 1.' });
+        }
+
+
+        const checkingInfo2 = await userToUser.findOne({ $and: [{ seller: buyer }, { buyer: seller }] });
+
+
+        if (checkingInfo2) {
+          console.log('checkingInfo2')
+          return res.status(400).json({ error: 'A record with this buyer and seller already exists 2.' });
+        }
+
+
+        if (!checkingInfo1 && !checkingInfo2) {
+          const result = await userToUser.insertOne(info);
+          console.log(result);
+
+          return res.status(201).json({ message: 'Record created successfully.', result });
+        }
+      } catch (error) {
+        if (error.code === 11000) {
+
+          console.error('Duplicate key error:', error.message);
+          return res.status(400).json({ error: 'A record with this seller and buyer already exists.' });
+        }
+        console.error('Error:', error);
+
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
+
+    // get dada from  seller AndBuyer Collections 
+    app.get('/sellerAndBuyerCollections', async (req, res) => {
+
+      const seller = req?.query?.seller
+      const buyer = req?.query?.buyer
+
+      const result1 = await userToUser.findOne({ $and: [{ seller: seller }, { buyer: buyer }] })
+      
+      if(result1){
+        return res.send(result1)
+      }
+
+      const result2 = await userToUser.findOne({ $and: [{ seller: buyer }, { buyer: seller }] })
+   
+      return res.send(result2)
+
+    });
+
+
+    app.post('/postChatUserToUser', async (req, res) => {
+
+
+
+      try {
+        const id = req?.query?.id;
+        const chat = req.body;
+
+
+        if (!id) {
+          return res.status(400).send('Email parameter is missing');
+        }
+
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            chat: chat,
+          },
+        };
+
+        const options = { upsert: true };
+
+        const updateResult = await userToUser.updateOne(filter, updateDoc, options);
+
+        console.log(updateResult)
+
+        if (updateResult.matchedCount === 0 && updateResult.upsertedCount === 0) {
+          return res.status(404).send('User not found');
+        }
+
+        res.send(updateResult);
+      } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('An error occurred');
+      }
+    }, []);
+
+// get all users chats
+
+app.get('/userAllChats', async (req, res) => {
+  try {
+    const email = req?.query?.email;
+    if (!email) {
+      throw new Error('Email is missing in the request query.');
+    }
+
+    const [userToUserResult, usersResult] = await Promise.all([
+      userToUser.aggregate([
+        {
+          $match: {
+            $or: [
+              { seller: email },
+              { buyer: email },
+            ],
+          },
+        },
+        {
+          $lookup: {
+            from: 'usersCollection', // Replace 'usersCollection' with the actual name of your 'users' collection
+            localField: 'seller',
+            foreignField: 'email',
+            as: 'sellerInfo',
+          },
+        },
+        {
+          $lookup: {
+            from: 'usersCollection', // Replace 'usersCollection' with the actual name of your 'users' collection
+            localField: 'buyer',
+            foreignField: 'email',
+            as: 'buyerInfo',
+          },
+        },
+      ]).toArray(),
+      usersCollection.find({ email: email }).toArray(),
+    ]);
+
+    const mergedResult = userToUserResult.concat(usersResult);
+
+    res.send(mergedResult);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
+
+
+
+    // app.get('/userAllChats', async (req, res) => {
+    //   try {
+    //     const email = req?.query?.email;
+    //     if (!email) {
+    //       throw new Error('Email is missing in the request query.');
+    //     }
+
+    //     const result = await userToUser.find({ $or: [{ seller: email }, { buyer: email }] }).toArray();
+
+
+
+
+    //     res.send(result);
+    //   } catch (error) {
+    //     console.error('Error:', error.message);
+    //     res.status(500).send('Internal Server Error');
+    //   }
+    // });
+
+ 
+
+    app.get('/getMessageUserToUser', async (req, res) => {
+      try {
+        const id = req.query.id;
+
+        
+        if (!id) {
+          return res.status(404).send('id not found');
+        }
+        const result = await userToUser.findOne({ _id: new ObjectId(id) });
+
+        if (!result) {
+          return res.status(404).send('Message not found');
+        }
+
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+      }
+    });
+
+
+    //  Real time Chat  User to User End by Tonmoy----------------------------------------------------------
 
 
 
